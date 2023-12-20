6f87ea526682e8770895b6e9e16dbe47f06b98ef
Miyayu <yuchan2215@gmail.com>
2023-12-20 14:48:32 +0900

    remove: 使用していない画面を削除

--- app/src/main/java/xyz/miyayu/attendancereader/MainActivity.kt
+++ app/src/main/java/xyz/miyayu/attendancereader/MainActivity.kt
@@ -3,6 +3,7 @@ package xyz.miyayu.attendancereader
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,7 +11,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SettingTestScreen()
+            Text(text = "Hello World")
         }
     }
 }
