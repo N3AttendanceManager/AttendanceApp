ec8ec044f49f6b1ab6a9b7f747855c8b5043bb07
Miyayu <yuchan2215@gmail.com>
2023-12-29 17:56:10 +0900

    add: EdgeToEdge対応

--- app/src/main/java/xyz/miyayu/attendancereader/MainActivity.kt
+++ app/src/main/java/xyz/miyayu/attendancereader/MainActivity.kt
@@ -3,6 +3,7 @@ package xyz.miyayu.attendancereader
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -16,6 +17,7 @@ import xyz.miyayu.attendancereader.view.route.LoginRoute
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             AttendanceReaderTheme {
                 Scaffold {
