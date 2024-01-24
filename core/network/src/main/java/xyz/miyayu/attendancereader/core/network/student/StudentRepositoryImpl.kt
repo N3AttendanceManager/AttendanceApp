c5c6d23b08406044a1e1fd983e2247486df1710c
Miyayu <yuchan2215@gmail.com>
2024-01-24 16:36:41 +0900

    WIP

--- core/network/src/main/java/xyz/miyayu/attendancereader/core/network/student/StudentRepositoryImpl.kt
+++ core/network/src/main/java/xyz/miyayu/attendancereader/core/network/student/StudentRepositoryImpl.kt
@@ -3,6 +3,7 @@ package xyz.miyayu.attendancereader.core.network.student
 import com.github.michaelbull.result.Err
 import com.github.michaelbull.result.Ok
 import com.github.michaelbull.result.Result
+import kotlinx.coroutines.async
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.decodeFromJsonElement
 import xyz.miyayu.attendancereader.core.network.NetworkService
@@ -27,7 +28,10 @@ class StudentRepositoryImpl @Inject constructor(
     }
 
     override suspend fun getStudents(departmentId: Int): Result<List<Student>, Throwable> {
-        TODO("Not yet implemented")
+        return com.github.michaelbull.result.coroutines.binding.binding {
+            val students = async { getAllStudent().bind() }
+            return@binding students.await().filter { it.departmentId == departmentId }
+        }
     }
 
     override suspend fun updateStudentIc(studentId: Int, icId: String): Result<Unit, Throwable> {
